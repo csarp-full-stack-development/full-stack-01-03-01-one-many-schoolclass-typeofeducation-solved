29bf3c3c0aa2e1e6680ce00c049d97e6f63fe712
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-04-02 22:12:48 +0200

    Dto pontosítás

--- Kreta.Shared/Dtos/TypeOfEducationDto.cs
+++ Kreta.Shared/Dtos/TypeOfEducationDto.cs
@@ -5,9 +5,6 @@ namespace Kreta.Shared.Dtos
     public class TypeOfEducationDto
     {
         public Guid Id { get; set; }
-
-        public bool HasId => Id != Guid.Empty;
-
         public string EducationName { get; set; } = string.Empty;
         public virtual ICollection<SchoolClass>? SchoolClasses { get; set; } = new List<SchoolClass>();
     }
